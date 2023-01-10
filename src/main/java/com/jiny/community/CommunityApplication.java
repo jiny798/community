2c5950a34b0190cfbfe514029f0beb301f633065
jiny <jyoung7988@naver.com>
2023-01-10 22:11:00 +0900

    commit test

--- src/main/java/com/jiny/community/CommunityApplication.java
+++ src/main/java/com/jiny/community/CommunityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommunityApplication {
 
 	public static void main(String[] args) {
-		//주석추가
 		SpringApplication.run(CommunityApplication.class, args);
 	}
 
