a9d56c366abf87a8da676f025ce9537af00d95ef
jyoung7988 <jyoung7988@naver.com>
2023-01-21 15:05:13 +0900

    로그인 사용자 정보 노출 구현

--- src/main/java/com/jiny/community/controller/HomeController.java
+++ src/main/java/com/jiny/community/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.jiny.community.controller;
 
+import com.jiny.community.domain.Account;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @GetMapping(value = "/")
-    public String getAddPostPage(){
+    public String home(HttpServletRequest request){
         return "board";
     }
 
