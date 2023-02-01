097067d6cf274fd29d77380e9c4fea36c7f5e122
jyoung7988 <jyoung7988@naver.com>
2023-02-01 17:15:56 +0900

    test

--- src/main/java/com/jiny/community/controller/admin/BoardManageController.java
+++ src/main/java/com/jiny/community/controller/admin/BoardManageController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -18,22 +20,30 @@ import java.util.List;
 public class BoardManageController {
     private final CategoryService categoryService;
 
-    @GetMapping(value = "")
+    @GetMapping(value = "/")
     public String getAdminPage(Model model){
         List<String> categorylist = categoryService.getCategoryNames();
 
+        log.info("카테고리 사이즈 = {}",categorylist.size());
         model.addAttribute("category_list",categorylist);
 
         return "admin/board";
     }
 
-
     @PostMapping(value = "/category")
     public String addCategory(String categoryName){
+        log.info("categoryName ={}",categoryName);
         categoryService.addCategory(categoryName);
-        return "admin/board";
-    }
 
+        return "redirect:/admin/";
+    }
+//    @PostMapping(value = "/category")
+//    public RedirectView addCategory(String categoryName){
+//        log.info("categoryName ={}",categoryName);
+//        categoryService.addCategory(categoryName);
+//
+//        return new RedirectView("/admin/");
+//    }
 
 
 }
